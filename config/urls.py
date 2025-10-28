d926df96fec74df7776bdbd0e841792f04f06ff8
xusniddinovk1 <kxprogrammer1@gmail.com>
2025-10-28 17:46:56 +0500

    initial commit

--- config/urls.py
+++ config/urls.py
@@ -5,10 +5,10 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('', include('language_selector.urls')),
     path('uz/', include(('medical.urls', 'medical'), namespace='uz')),
-    # path('ru/', include(('medical_ru.urls', 'medical_ru'), namespace='ru')),
+    path('ru/', include(('medical_ru.urls', 'medical_ru'), namespace='ru')),
     path('en/', include(('medical_en.urls', 'medical_en'), namespace='en')),
     path('uz/admin/', include(('dashboard.urls', 'dashboard'), namespace='uz-admin')),
-    # path('ru/admin/', include(('dashboard_ru.urls', 'dashboard_ru'), namespace='ru-admin')),
+    path('ru/admin/', include(('dashboard_ru.urls', 'dashboard_ru'), namespace='ru-admin')),
     path('en/admin/', include(('dashboard_en.urls', 'dashboard_en'), namespace='en-admin')),
 ]
 
